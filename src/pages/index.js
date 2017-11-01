1f5d96d1805efae2e6d33e5c1229632a79d0d63c
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-11-01 12:51:22 +0300

    minimal home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
 import React from 'react'
+import './index.css'
 
-export default () => <div>Coming soon!</div>
+export default () => (
+	<div>
+		<img
+			src="https://avatars0.githubusercontent.com/u/12673605"
+			width="120"
+			height="120"
+		/>
+		<h1>Ahmed T. Ali</h1>
+		<p>OSS Enthusiast Software Engineer</p>
+		<ul>
+			<li>
+				<a href="https://github.com/ahmed-taj"> GitHub</a>
+			</li>
+			<li>
+				<a href="https://twitter.com/ah_tajelsir"> Twitter</a>
+			</li>
+			<li>
+				<a href="https://medium.com/ahmed-t-ali"> Blog</a>
+			</li>
+		</ul>
+	</div>
+)
