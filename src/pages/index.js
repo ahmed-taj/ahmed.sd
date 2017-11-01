04622c87b0d69e8a65095a6bc792fcc9d2f7927d
Ahmed T. Ali <ah.tajelsir@gmail.com>
2017-11-01 14:07:19 +0300

    fix page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,34 @@
+// Packages
 import React from 'react'
+
+// Ours
 import './index.css'
 
 export default () => (
 	<div>
-		<img
-			src="https://avatars0.githubusercontent.com/u/12673605"
-			width="120"
-			height="120"
-		/>
-		<h1>Ahmed T. Ali</h1>
-		<p>OSS Enthusiast Software Engineer</p>
-		<ul>
-			<li>
-				<a href="https://github.com/ahmed-taj"> GitHub</a>
-			</li>
-			<li>
-				<a href="https://twitter.com/ah_tajelsir"> Twitter</a>
-			</li>
-			<li>
-				<a href="https://medium.com/ahmed-t-ali"> Blog</a>
-			</li>
-		</ul>
+		<head>
+			<title>Ahmed T. Ali</title>
+		</head>
+		<section>
+			<img
+				src="https://avatars0.githubusercontent.com/u/12673605"
+				width="120"
+				height="120"
+				alt="A photo of me"
+			/>
+			<h1>Ahmed T. Ali</h1>
+			<p>OSS Enthusiast Software Engineer</p>
+			<ul>
+				<li>
+					<a href="https://github.com/ahmed-taj"> GitHub</a>
+				</li>
+				<li>
+					<a href="https://twitter.com/ah_tajelsir"> Twitter</a>
+				</li>
+				<li>
+					<a href="https://medium.com/ahmed-t-ali"> Blog</a>
+				</li>
+			</ul>
+		</section>
 	</div>
 )
