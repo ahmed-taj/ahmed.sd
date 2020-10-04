93bafefccc7f9b06cb467117106ee856370d9afe
Ahmed T. Ali <ah.tajelsir@gmail.com>
2020-10-04 13:51:41 +0200

    build(css): enable Purge CSS only in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
 module.exports = {
 	plugins: [
+		// Tailwind CSS
+		// https://tailwindcss.com/
 		require("tailwindcss"),
+
+		// Autoprefixer
+		// https://autoprefixer.github.io/
 		require("autoprefixer"),
+
+		// cssnano
+		// https://cssnano.co/
 		require("cssnano"),
 	],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+const isProd = process.env.HUGO_ENVIRONMENT === "production";
+
 module.exports = {
 	future: {
 		removeDeprecatedGapUtilities: true,
 		purgeLayersByDefault: true,
 	},
 	purge: {
-		enabled: true,
+		enabled: isProd,
 		content: ["./layouts/**/*.html"],
 	},
 	theme: {
